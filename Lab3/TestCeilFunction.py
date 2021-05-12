0f3ff7bee68a34171966983c98b99cf883d465a4
Luis Montes <luised.montes.siordia@gmail.com>
2021-05-12 00:48:43 -0500

    Fixed issue with isinstance function

--- Lab3/TestCeilFunction.py
+++ Lab3/TestCeilFunction.py
@@ -29,7 +29,7 @@ class TestMathCeilFunction(unittest.TestCase):
 
     @unittest.skipIf(sys.version_info >= (3,0), "Python3 returns an integer value")
     def test_is_float(self):        
-        self.assertTrue(isinstance(math.ceil(1.1)) == float)
+        self.assertTrue(isinstance(math.ceil(1.1), float))
     
     def test_very_high_value(self):
         self.assertEqual(math.ceil(9999999999999999999999.1), 10000000000000000000000)
